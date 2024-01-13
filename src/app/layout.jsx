d8e5147bc9f338e07d5b7e3b4693bdf378dd7d94
devjunaeid <juaneid.md48@gmail.com>
2024-01-14 00:14:24 +0600

    Removed: Unused import;  Google Fonts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import "./globals.css";
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Synafeia",
@@ -12,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Navbar />
         <div className="mainContainer">
         <main>{children}</main>
